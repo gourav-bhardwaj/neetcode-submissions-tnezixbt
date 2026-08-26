269582ac0f2a5d212a0bb84fdb877e141094e9ce
Gourav kumar <47908345+gourav-bhardwaj@users.noreply.github.com>
2026-08-26 08:27:52 +0530

    Add: anagram-groups - submission-18

---
+++ Data Structures & Algorithms/anagram-groups/submission-18.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        
+        Map<String, List<String>> mapOfList = new HashMap<>();
+        for(int i = 0; i < strs.length; i++) {
+            int[] freqArray = new int[26];
+            for(int j = 0; j < strs[i].length(); j++) {
+                freqArray[strs[i].charAt(j) - 'a']++;
+            }
+            String key = Arrays.toString(freqArray);
+            mapOfList.putIfAbsent(key, new ArrayList<>());
+            mapOfList.get(key).add(strs[i]);
+        }
+        return new ArrayList<>(mapOfList.values());
+    }
+}
